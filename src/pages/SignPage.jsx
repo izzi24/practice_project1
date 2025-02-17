77450df2252fe8c5b29b4dc61f4b8b2688df9247
izzi24 <adedapotope57@gmail.com>
2025-02-17 18:50:41 +0100

    micer

--- src/pages/SignPage.jsx
+++ src/pages/SignPage.jsx
@@ -1,14 +1,14 @@
-import  { useState } from "react";
+import { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import userInfo from '../data/userData.json'
+import userInfo from "../data/userData.json";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 export default function SignPage() {
   const [username, setUsername] = useState("");
   let [passwd, setPasswd] = useState("");
-  const navigate = useNavigate()
-  let [showPassword, setShowPassword] = useState(false)
+  const navigate = useNavigate();
+  let [showPassword, setShowPassword] = useState(false);
 
   const changeUsername = (e) => {
     setUsername(e.target.value);
@@ -26,34 +26,29 @@ export default function SignPage() {
     // await login(data).then((res)=>{
     //   localStorage.setItem("user_token", res.data)
     // })
-    
-    if (username === userInfo[0].username && passwd === userInfo[0].password ) {
-      toast.success('user login successful')
+
+    if (username === userInfo[0].username && passwd === userInfo[0].password) {
+      toast.success("user login successful");
       console.log(userInfo[0]);
       const data = {
         id: userInfo[0].id,
         username: userInfo[0].username,
         gender: userInfo[0].gender,
         location: userInfo[0].location,
-        role: userInfo[0].role
-      }
-      localStorage.setItem("user_token", JSON.stringify(data))
-      navigate('/')
-      window.location.reload()
-
+        role: userInfo[0].role,
+      };
+      localStorage.setItem("user_token", JSON.stringify(data));
+      navigate("/");
+      window.location.reload();
     } else {
-      toast.error('Incorrect credentials')
-      setPasswd('')
+      toast.error("Incorrect credentials");
+      setPasswd("");
     }
-      
-    
-
-  }
+  };
   return (
-    <div className="">
-    <Header />
-      
-      <div className=" lg:flex hidden items-center bg-gradient-to-r from-violet-500 to-fuchsia-500 justify-center py-3 w-full">
+    <div className=" ">
+      <Header/>
+      <div className=" lg:flex hidden items-center justify-center py-3 w-full">
         <div className="bg-white text-center rounded-2xl h-[75vh] w-[50%]">
           <p className="  py-4 capitalize text-3xl font-bold">welcome back!</p>
           <p className="capitalize text-2xl font-bold">login</p>
@@ -65,20 +60,35 @@ export default function SignPage() {
               value={username}
               onChange={(e) => changeUsername(e)}
             />
-            <input
-              className=" border h-[40px] mt-5 w-[90%]"
-              type="password"
-              placeholder="Type your password"
-              value={passwd}
-              onChange={(e) => changePasswd(e)}
-            />
+            <div className="relative">
+              <input
+                className="  border h-[40px] mt-5 px-3 w-[90%]"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                value={passwd}
+                onChange={(e) => changePasswd(e)}
+              />
+              <button
+                type="button"
+                className="absolute right-10 top-7 text-gray-400"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <i
+                  className={`pi ${showPassword ? "pi-eye" : "pi-eye-slash"}`}
+                ></i>
+              </button>
+            </div>
           </div>
           <div className="text-end text-sm font-bold lg:px-12 sm:px-5">
             {" "}
             <p>Forget password?</p>
           </div>
           <div className="py-5">
-            <button onClick={handleSubmit} type="button" className="py-3 px-9 text-white rounded-xl text-xl bg-gradient-to-r from-violet-500 to-fuchsia-500">
+            <button
+              onClick={handleSubmit}
+              type="button"
+              className="py-3 px-9 text-white rounded-xl text-xl bg-gradient-to-r from-violet-500 to-fuchsia-500"
+            >
               Login
             </button>
           </div>
@@ -90,31 +100,37 @@ export default function SignPage() {
           </div>
         </div>
       </div>
-      <div className=" lg:hidden sm:block items-center px-5 bg-gradient-to-r from-violet-500 to-fuchsia-500 justify-center py-3 w-full">
-        <div className="bg-white text-center rounded-2xl w-[full]  h-[vh]">
+      <div className=" lg:hidden sm:visible items-center  px-5 bg-gray-600 justify-center py-3 ">
+        <div className="bg-white text-center rounded-2xl w-[full]  ">
           <p className="  py-4 capitalize text-3xl font-bold">welcome back!</p>
           <p className="capitalize text-2xl font-bold">login</p>
           <div className="p-2">
-           <div>
-           <input
-              className=" border w-[90%] px-3 h-[40px] mt-5"
-              type="text"
-              placeholder="Type your username"
-              value={username}
-              onChange={(e) => changeUsername(e)}
-            />
-           </div>
-           <div className="relative">
-            <input
-              className="  border h-[40px] mt-5 px-3 w-[90%]"
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter your password"
-              value={passwd}
-              onChange={(e) => changePasswd(e)}
-            />
-            <button type="button" className="absolute right-10 top-7 text-gray-400" onClick={() => setShowPassword(!showPassword)}>
-              <i className={`pi ${showPassword ? "pi-eye" : "pi-eye-slash"}`}></i>
-            </button>
+            <div>
+              <input
+                className=" border w-[90%] px-3 h-[40px] mt-5"
+                type="text"
+                placeholder="Type your username"
+                value={username}
+                onChange={(e) => changeUsername(e)}
+              />
+            </div>
+            <div className="relative">
+              <input
+                className="  border h-[40px] mt-5 px-3 w-[90%]"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                value={passwd}
+                onChange={(e) => changePasswd(e)}
+              />
+              <button
+                type="button"
+                className="absolute right-10 top-7 text-gray-400"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <i
+                  className={`pi ${showPassword ? "pi-eye" : "pi-eye-slash"}`}
+                ></i>
+              </button>
             </div>
           </div>
           <div className="text-end text-sm font-bold lg:px-12 sm:px-5">
@@ -122,7 +138,11 @@ export default function SignPage() {
             <p className="px-7">Forget password?</p>
           </div>
           <div className="py-5">
-            <button onClick={handleSubmit} type="button" className="py-3 px-9 text-white rounded-xl text-xl bg-gradient-to-r from-violet-500 to-fuchsia-500">
+            <button
+              onClick={handleSubmit}
+              type="button"
+              className="py-3 px-9 text-white rounded-xl text-xl bg-gradient-to-r from-violet-500 to-fuchsia-500"
+            >
               Login
             </button>
           </div>
@@ -134,9 +154,7 @@ export default function SignPage() {
           </div>
         </div>
       </div>
-      <div>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 }
